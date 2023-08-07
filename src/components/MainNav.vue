140958d98f5edc7deac07a295634c1725b0140c4
Vadim Gadjimuradov <gadjimuradov.vadim@reliableopensource.com>
2023-08-07 20:52:45 +0300

    initial commit

---
+++ src/components/MainNav.vue
@@ -0,0 +1,51 @@
+<template>
+  <header class="w-full text-sm ">
+    <div class="fixed top-0 left-0 w-full h-16 bg-white">
+      <div
+        class="flex flex-nowrap gap-6 h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
+        <a class="flex h-full items-center text-xl" :href="url">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li v-for="menuItem in menuItems" :key="menuItem"
+              class="h-full ml-9 first:ml-0">
+              <a href="#" class="flex h-full items-center py-2.5">{{ menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
+        <div class="flex items-center h-full ml-auto">
+          <profile-image v-if="isLoggedIn" />
+          <action-button v-else @click="loginUser" />
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import ActionButton from "@/components/ActionButton.vue";
+import ProfileImage from "@/components/ProfileImage.vue";
+export default {
+  name: "MainNav",
+  components: { ActionButton, ProfileImage},
+  data() {
+    return {
+      company: 'Vue Careers',
+      url: 'https://careers.google.com/',
+      menuItems: [
+        'Teams',
+        'Location',
+        'Life of Vue',
+        'Students',
+        'Jobs'
+      ],
+      isLoggedIn: false
+    }
+  },
+  methods: {
+    loginUser (){
+      this.isLoggedIn = true
+    }
+  }
+}
+</script>
---
+++ tests/unit/components/MainNav.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/vue'
+import MainNav from '@/components/MainNav.vue'
+import userEvent from '@testing-library/user-event'
+
+describe('MainNav', () => {
+  it('displays company name', () => {
+    render(MainNav)
+    const companyName = screen.getByText('Vue Careers')
+    expect(companyName).toBeInTheDocument()
+  })
+
+  it('displays mneu items for navigation', () => {
+    render(MainNav)
+    const navigationMenuItems = screen.getAllByRole('listitem')
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.textContent)
+    expect(navigationMenuTexts).toEqual(['Teams', 'Location', 'Life of Vue', 'Students', 'Jobs'])
+  })
+
+  describe('when the user is logged in', () => {
+    it('displays user profile picture', async () => {
+      render(MainNav)
+
+      let profileImage = screen.queryByRole('img', { name: /Profile image/i })
+      expect(profileImage).not.toBeInTheDocument()
+
+      const logginButton = screen.getByRole('button', { name: /Sign in/i })
+      await userEvent.click(logginButton)
+
+      profileImage = screen.queryByRole('img', { name: /Profile image/i })
+      expect(profileImage).toBeInTheDocument()
+    })
+  })
+})
