0c8a83747cabdce5a9b29f6e572a47a5041b302e
Vadim Gadjimuradov <gadjimuradov.vadim@reliableopensource.com>
2023-08-30 21:56:23 +0300

    Add styles for form

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -2,6 +2,26 @@
   <form
     class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3">
     <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+
+    <div class="flex h-full flex-1 flex-nowrap text-base font-light">
+      <div class="relative flex h-full flex-1 items-center pr-3">
+        <label class="absolute -top-10 left-0">Role</label>
+        <input type="text" placeholder="Software engineer"
+          class="w-full text-lg font-normal focus:outline-none" />
+      </div>
+
+      <span
+        class="flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3">
+        in
+      </span>
+
+      <div class="relative flex h-full flex-1 items-center pl-3">
+        <label class="absolute -top-10 left-0">Where?</label>
+        <input type="text" placeholder="Los Angeles"
+          class="w-full text-lg font-normal focus:outline-none" />
+      </div>
+    </div>
+
   </form>
 </template>
 
